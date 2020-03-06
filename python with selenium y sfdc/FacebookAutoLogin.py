f78189322c5db023564e5d81d79d8a48088f4051
Martin Noy <mnoy@labsxd.com>
2020-03-06 10:04:20 -0300

    Cambio Pantalla Maximizada

--- python with selenium y sfdc/FacebookAutoLogin.py
+++ python with selenium y sfdc/FacebookAutoLogin.py
@@ -4,6 +4,7 @@ user_name = "tincho_12db@hotmail.com"
 password = "41142737Noy"
 driver = webdriver.Chrome()
 driver.get("https://www.facebook.com")
+driver.maximize_window()
 element = driver.find_element_by_id("email")
 element.send_keys(user_name)
 element = driver.find_element_by_id("pass")
